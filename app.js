a5ee0cd83737c35892e19162e2ba72c5c9358f19
iep21 <ianemmanuelpardo@gmail.com>
2023-02-13 01:17:22 +0800

    valentines

---
+++ app.js
@@ -0,0 +1,60 @@
+window.addEventListener("load", function() {
+    setTimeout(function() {
+      document.getElementById("loader").style.display = "none";
+      document.getElementById("page-content").classList.remove("hidden");
+    }, 2000); // Change 2000 to the number of milliseconds you want the loading animation to display
+
+    // Get references to the button and modal elements
+    const button = document.getElementById('play-video-button');
+    const modal = document.getElementById('video-modal');
+
+    // Add a click event listener to the button
+    button.addEventListener('click', () => {
+    // Show the modal
+    modal.style.display = 'inline-block';
+    content.style.display = 'none';
+    document.cookie = "cookieName=cookieValue; SameSite=None; Secure";
+    });
+
+    // Get a reference to the close button
+    const closeButton = document.getElementById('close-video-button');
+
+    // Add a click event listener to the close button
+    closeButton.addEventListener('click', () => {
+    // Hide the modal
+    modal.style.display = 'none';
+    content.style.display = 'block';
+    });
+
+  });
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    // Your code goes here
+    // Define a function to create a heart element
+  function createHeart() {
+    const heart = document.createElement("div");
+    heart.classList.add("heart");
+    heart.style.left = Math.random() * window.innerWidth + "px";
+    document.getElementById("heart-container").appendChild(heart);
+    // Remove the heart element after it falls offscreen
+    setTimeout(() => heart.remove(), 5000);
+  }
+  
+  // Call the createHeart function every 200 milliseconds to create a new heart
+  setInterval(createHeart, 100);     
+    
+  });
+
+  const toggleButton = document.getElementById('toggleButton');
+  const content = document.getElementById('content');
+
+  toggleButton.addEventListener('click', function() {
+    if (content.style.display === 'none') {
+      content.style.display = 'block';
+      toggleButton.style.display = 'none';
+      hide.style.display = 'none';
+    } else {
+        content.style.display = 'none';
+        toggleButton.textContent = 'Click me';
+    }
+  });
